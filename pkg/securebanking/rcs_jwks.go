d72b584924ea8ce5410c0cc8f613ad688aac5a7e
Dan Badham <dan.badham@forgerock.com>
2023-01-31 11:23:48 +0000

    779: Configuring AM Remote Consent from environment variables
    Added logic to create a JWKS from an RSA public key. Configuring this JWKS in AM for it to verify JWTs signed by the RCS.
    
    Mounted the public signing key from the rcs-signing secret.
    Configured environment variables for the signing keyId and jwt issuer, so that AM and RCS are configured consistently.
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/779

---
+++ pkg/securebanking/rcs_jwks.go
@@ -0,0 +1,46 @@
+package securebanking
+
+import (
+	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-jose/go-jose/v3"
+)
+
+func CreateRcsJwks(rsaPublicKey string, keyId string) []byte {
+	rsaPubKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(rsaPublicKey))
+	if err != nil {
+		panic(err)
+	}
+	jwk := jose.JSONWebKey{
+		KeyID:     keyId,
+		Key:       rsaPubKey,
+		Algorithm: "RSA",
+		Use:       "sig",
+	}
+
+	// API provides no way to build a JWKS and marshall it, so we need to do that manually
+	jwkRawJson, err := jwk.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshall the JWK back to a map
+	var jwkJson map[string]interface{}
+	json.Unmarshal(jwkRawJson, &jwkJson)
+
+	// Wrap our key as a single item in a slice
+	jwkSlice := []map[string]interface{}{
+		jwkJson,
+	}
+
+	// Produce the JWKS containing the slice
+	jwks := map[string]interface{}{
+		"keys": jwkSlice,
+	}
+
+	jwksMarshalled, err := json.Marshal(jwks)
+	if err != nil {
+		panic(err)
+	}
+	return jwksMarshalled
+}
