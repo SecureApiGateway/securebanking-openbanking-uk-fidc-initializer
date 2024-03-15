e0c7a6e6643a17e19c4fe58b65e54232f3de1b5a
Shaun Harrison <shaun.harrison@forgerock.com>
2024-03-15 14:11:35 +0000

    1319: Remove if statement in main.go as being done within function

--- main.go
+++ main.go
@@ -108,12 +108,10 @@ func main() {
 	
 	fmt.Println("Attempt to create OIDC claims script..")
 	id := securebanking.CreateOIDCClaimsScript(session.Cookie)
-	if common.Config.Environment.SapigType == "ob" {
-		fmt.Println("Attempt to Create OB OAUTH2 Providers...")
-		securebanking.UpdateOAuth2Provider(id)
-	} else {
-		fmt.Println("Attempt to Create Core OAUTH2 Providers...")
-	}
+	
+	fmt.Println("Attempt to Create OAUTH2 Providers...")
+	securebanking.UpdateOAuth2Provider(id)
+	
 	securebanking.CreateBaseURLSourceService(session.Cookie)
 
 	time.Sleep(5 * time.Second)
